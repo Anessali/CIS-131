09afc0f38c3826e5e1c82402ec1468d9c2c7f8ac
Unknown <liggettt@otc.edu>
2017-09-14 07:22:05 -0500

    Morning edits

---
+++ Image Swapping/swap.js
@@ -0,0 +1,14 @@
+var pictures = ["images/cat.jpg", "images/color.jpg"];
+
+function ImageSwap() {
+    var count = 0;
+    var imgName;
+    //imgName = document.getElementById("cImage").src;
+    
+}
+
+function Swap(){
+    
+}
+document.getElementById(cImage).addEventListener("click", )
+//document.write();
