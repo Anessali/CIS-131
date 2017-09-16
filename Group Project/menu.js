d2424c40c6597f4d277c3c30bf68518a80fb8893
Unknown <liggettt@otc.edu>
2017-09-16 13:34:51 -0500

    More edits in tlc

--- Group Project/menu.js
+++ Group Project/menu.js
@@ -1,31 +1,31 @@
 var menuHeaders = ['Bracket Burgers', 'Pastries', 'Coffee'];
 
+var menuCount = 1; //global count variable for PopulateMenu
 var itemCount = 1; //global count variable for PopulateMenuItems
 
 function PopulateMenu(){
-    var count = 1;
     var leMenu;
     do { //populates menu items
       leMenu = ( //adds menu elements into variable
         '<div class="row">' +
           '<div class="col-md-12 centered" id="menu-"><h2>' +
-            //'Bracket Burgers' +
-            menuHeaders[count - 1] + //1 subtracted for array
-          '</h2></div>' + 
+            menuHeaders[menuCount - 1] + //1 subtracted for array
+          '</h2></div>' +
         '</div>' +
-        '<div class="col-md-4 centered" id="menuItem-"' + itemCount + '>' +
+        '<div class="col-md-4 centered"' + '"id="menuItem-' + itemCount + '">' +
         '</div>'
       );
-      document.getElementById("menu-" + count).innerHTML = leMenu;
+      document.getElementById("menu-" + menuCount).innerHTML = leMenu;
       PopulateMenuItems();
-      count++;
-  } while (count <= 3);
+      menuCount++;
+  } while (menuCount <= 3);
 }
 
 function PopulateMenuItems(){
     var leMenu;
     do { //populates menu items
       leMenu = ( //adds menu elements into variable
+        '<div class="col-md-4 centered" class="menuItem-">' + itemCount +
           '<div class="row">' +
             '<div class="col-sm-6 col-md-12">' +
               '<div class="thumbnail">' +
@@ -37,15 +37,21 @@ function PopulateMenuItems(){
                 '</div>' +
               '</div>' +
             '</div>' +
-          '</div>'
+          '</div>' +
+        '</div'
       );
-      document.getElementById("menuItem-" + itemCount).innerHTML = leMenu;
-      count++;
-  } while (count <= 6);
+      var getClass = document.getElementsByClassName("menuItem-" + itemCount);
+      getClass[0].innerHTML = leMenu;
+      getClass[1].innerHTML = leMenu;
+      getClass[2].innerHTML = leMenu;
+      //document.getElementsById("menuItem-" + itemCount).innerHTML = leMenu;
+      itemCount++;
+  } while (itemCount <= 6);
 }
 
 function StartScripts(){
-    PopulateMenu();
+    //PopulateMenu();
+    PopulateMenuItems();
 }
 
 window.addEventListener("load", StartScripts, false)
