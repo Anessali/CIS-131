7145d62bb31b52a5d8de1cde9662d887fee128d4
Unknown <liggettt@otc.edu>
2017-09-18 17:52:56 -0500

    TLC Edits for IS

--- Image Swapping/swap.js
+++ Image Swapping/swap.js
@@ -1,12 +1,12 @@
-var pictures = ['<img src="images/cat.jpg">', '<img src="images/color.jpg">', '<img src="images/city.jpg">', '<img src="images/sea.jpg">'];
+var pictures = ['<img src="images/cat.jpg" onclick="IcoClick(this.id)">', '<img src="images/color.jpg" onclick="IcoClick(this.id)">',
+'<img src="images/city.jpg" onclick="IcoClick(this.id)">', '<img src="images/sea.jpg" onclick="IcoClick(this.id)">'];
 
 function DisplayImage() {
     var count = 0;
-    var swap;
     var imgLoc;
+    var swap; //icon population variable
     imgLoc = document.getElementById("displayPic");
     imgLoc.innerHTML = pictures[count];
-
     do { //generates page icons
       swap = document.getElementById("icon-" + count);
       swap.innerHTML = pictures[count];
@@ -14,13 +14,13 @@ function DisplayImage() {
     } while(count <= 3);
 }
 
-function PicClick() {
-
+function IcoClick(clickImg) {
+    alert(clickImg);
 }
 
 function StartScripts(){
     DisplayImage();
-    PicClick();
 }
 
-window.addEventListener("load", StartScripts, false)
+window.addEventListener("load", StartScripts, false);
+document.addEventListener("click", IcoClick(), false);
