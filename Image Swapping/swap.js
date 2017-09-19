53857fdeebcdf52c294cf2af105f552724ca1af6
Unknown <liggettt@otc.edu>
2017-09-18 21:15:06 -0500

    Final edits IS

--- Image Swapping/swap.js
+++ Image Swapping/swap.js
@@ -1,5 +1,5 @@
-var pictures = ['<img src="images/cat.jpg" onclick="IcoClick(this.id)">', '<img src="images/color.jpg" onclick="IcoClick(this.id)">',
-'<img src="images/city.jpg" onclick="IcoClick(this.id)">', '<img src="images/sea.jpg" onclick="IcoClick(this.id)">'];
+var pictures = ['<img src="images/cat.jpg" onclick="ClickOne()">', '<img src="images/color.jpg" onclick="ClickTwo()">',
+'<img src="images/city.jpg" onclick="ClickThree()">', '<img src="images/sea.jpg" onclick="ClickFour()">'];
 
 function DisplayImage() {
     var count = 0;
@@ -13,14 +13,44 @@ function DisplayImage() {
       count++;
     } while(count <= 3);
 }
-
-function IcoClick(clickImg) {
-    alert(clickImg);
+/****************************************
+*                Timer                  *
+****************************************/
+//variables
+var timeCount = 0;
+timer = setInterval(ImageSlider, 3000);
+//function
+function ImageSlider(){
+    var imgSlide = document.getElementById("displayPic");
+    imgSlide.innerHTML = pictures[timeCount];
+    timeCount++;
+    if (timeCount >= 4){
+        timeCount = 0;
+    }
 }
 
+/****************************************
+*             Swaps Image               *
+****************************************/
+function ClickOne() {
+    var icoImg = document.getElementById("displayPic");
+    icoImg.innerHTML = pictures[0];
+}
+function ClickTwo() {
+    var icoImg = document.getElementById("displayPic");
+    icoImg.innerHTML = pictures[1];
+}
+function ClickThree() {
+    var icoImg = document.getElementById("displayPic");
+    icoImg.innerHTML = pictures[2];
+}
+function ClickFour() {
+    var icoImg = document.getElementById("displayPic");
+    icoImg.innerHTML = pictures[3];
+}
+
+//Starts script
 function StartScripts(){
     DisplayImage();
 }
-
 window.addEventListener("load", StartScripts, false);
-document.addEventListener("click", IcoClick(), false);
