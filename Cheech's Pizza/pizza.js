790dbe2f545b50cd59a29cf20b68ccecd4afcbb4
Unknown <liggettt@otc.edu>
2017-09-29 12:35:35 -0500

    Functional Select

--- Cheech's Pizza/pizza.js
+++ Cheech's Pizza/pizza.js
@@ -1,23 +1,32 @@
+/* jshint browser: true */
 
+/***************************
+ * Edit these variables to *
+ * control page elements   *
+ ***************************/
+var numInList = 10; //controls number of choices in 'How many?' Select
+var pizzaType = ['Cheese'];
 
 /***************************
  * Function populates page *
  ***************************/
-function PopulatePage(){
-    var pItem = '<option>' + count + '</option>';
-    for (var count = 1; count <= 10; count++){
-        document.getElementsByClassName("form-control")[0].innerHTML = pItem;
+function PopulatePage() {
+    
+    var idItem;
+    var listItem;
+    /*   loop populates the 'How many?' Select   */
+    for (var count = 1; count <= numInList; count++){
+        //adds option elements with item ID
+        idItem = '<option id="item-' + count + '"></option>';
+        document.getElementById('numList').innerHTML += idItem;
+        //populates option elements with numbers
+        listItem = document.getElementById('item-' + count);
+        listItem.innerHTML += count;
     }
 }
 
-function Test(){
-    document.getElementsByTagName('h1')[0].innerHTML = 'Hello. This be javascript';
-}
-
 function Start(){
-    //var test = 'troubleshoot'
     PopulatePage();
-    //Test();
 }
 
 window.addEventListener("load", Start(), false);
