3283422ac170eb7e9f7ddca9c428aa557db8d2b4
Unknown <liggettt@otc.edu>
2017-09-29 05:45:56 -0500

    Morning push

--- Cheech's Pizza/pizza.js
+++ Cheech's Pizza/pizza.js
@@ -1,9 +1,23 @@
+
+
+/***************************
+ * Function populates page *
+ ***************************/
 function PopulatePage(){
-    document.getElementsByClassName
+    var pItem = '<option>' + count + '</option>';
+    for (var count = 1; count <= 10; count++){
+        document.getElementsByClassName("form-control")[0].innerHTML = pItem;
+    }
+}
+
+function Test(){
+    document.getElementsByTagName('h1')[0].innerHTML = 'Hello. This be javascript';
 }
 
 function Start(){
-    
+    //var test = 'troubleshoot'
+    PopulatePage();
+    //Test();
 }
 
 window.addEventListener("load", Start(), false);
