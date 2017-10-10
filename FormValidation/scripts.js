c29b02d318c2f563d496b853e7a55551d85865fb
ShiroNya <liggetttj@gmail.com>
2017-10-10 00:08:27 -0500

    final edits tonight

--- FormValidation/scripts.js
+++ FormValidation/scripts.js
@@ -2,15 +2,34 @@
 
 //populates the listbox
 function popSelect(){
-    var selects = ["Test", "Test2"];
+    //var selects = ["Test", "Test2"];
     var selectHolder = document.getElementById("listbox");
-    for (var i = 0; i < selects.length; i++){
-        selectHolder.innerHTML += '<option value="' + i + '" id="item-' + i + '">' + selects[i] + '</option>';
+    for (var i = 1; i <= 5; i++){
+        selectHolder.innerHTML += '<option>' + i + '</option>';
     }
 }
 
+//form validation
+function submitted(){
+    //var name = document.getElementById("name").value;
+    //var email = document.getElementById("email").value;
+    //var radio = document.getElementById("radioB").value;
+    //var listNum = document.getElementById("listbox").value;
+    document.getElementById("name").style.backgroundColor = "red";
+    if (name.length < 6){
+        //document.style.display();
+
+    }
+}
+
+function reg(){
+    //const string
+}
+
 function start(){
+
     popSelect();
 }
 
 window.addEventListener("load", start, false);
+document.addEventListener("submit", submitted, false);
