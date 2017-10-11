cd7233807ac22df004ac5c4790c4fd265aa605e0
LIGGETT <liggettt@otc.edu>
2017-10-11 09:41:09 -0500

    Kirsten lab time

--- FormValidation/scripts.js
+++ FormValidation/scripts.js
@@ -1,5 +1,4 @@
 
-
 //populates the listbox
 function popSelect(){
     //var selects = ["Test", "Test2"];
@@ -10,23 +9,58 @@ function popSelect(){
 }
 
 //form validation
-function submitted(){
-    //var name = document.getElementById("name").value;
-    //var email = document.getElementById("email").value;
-    //var radio = document.getElementById("radioB").value;
-    //var listNum = document.getElementById("listbox").value;
-}
-
 function validate(){
 
-    if (name.length < 2){
-        document.getElementById("name").style.backgroundColor = "red";
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var em = /@/.test(email);
+    var phone = document.getElementById("phone").value;
+    var ph = /^\d{3}-\d{3}-\d{4}$/.test(phone);
+    var radio = document.getElementById("radioB").value;
+    var listNum = document.getElementById("listbox").value;
+    var error = document.getElementsByClassName("error");
+    var namebool = false, emailBool = false, phoneBool = false;
+    //Name
+    if (name.length < 6){
+        document.getElementById("name").style.backgroundColor = "#ffcccc";
+        error[0].innerHTML = "You must enter a valid name";
+    } else {
+        document.getElementById("name").style.backgroundColor = "";
+        error[0].innerHTML = "";
+        nameBool = true;
     }
+    //Email
+    if (em == false){
+        document.getElementById("email").style.backgroundColor = "#ffcccc";
+        error[1].innerHTML = "Invalid email address";
+    } else {
+        document.getElementById("email").style.backgroundColor = "";
+        error[1].innerHTML = "";
+        emailBool = true;
+    }
+    //Phone Number
+    if (ph == false){
+        document.getElementById("phone").style.backgroundColor = "#ffcccc";
+        error[2].innerHTML = "Invalid phone number";
+    } else {
+        document.getElementById("phone").style.backgroundColor = "";
+        error[2].innerHTML = "";
+        phoneBool = true;
+    }
+    if (nameBool && emailBool && phoneBool){
+        document.getElementById('vetForm').submit();
+        window.location.href = "thankyou.html";
+    }
+}
+
+function checkRadio(){
+    var radio = document.getElementsByName('animal');
+    radio[0].checked = true;
 }
 
 function start(){
     popSelect();
+    checkRadio();
 }
 
 window.addEventListener("load", start, false);
-//document.addEventListener("click", submitted, false);
