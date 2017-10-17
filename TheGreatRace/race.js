1106e03626217160f42e4831ed7633094663a966
Shiro <shiro@localhost.localdomain>
2017-10-16 21:14:54 -0500

    some edits

--- TheGreatRace/race.js
+++ TheGreatRace/race.js
@@ -1,7 +1,16 @@
+//Racers
+var chrome = document.getElementById("chrome");
+var firefox = document.getElementById("firefox");
+var ie = document.getElementById("exploder");
+//Counts
+var count = 0;
 
 
-function start(){
-    
+function lights(){
+    document.getElementById("sun").src = "images/sunGreen.png";
+    startRace();
 }
 
-document.addEventListener("load", start, false);
+function startRace(){
+    firefox.innerHTML = style.left += 1000;
+}
